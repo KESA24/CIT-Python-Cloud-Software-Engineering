436f64fb64121a6c5815fd0336f57483fd1d0d48
kesa <nekepatri@gmail.com>
2021-11-23 13:41:32 +0300

    Week 3 day 2, while loops

---
+++ whileLoops.py
@@ -0,0 +1,26 @@
+x = 1
+while x!= 10:
+    print(x)
+    break
+
+i = 0
+while i== 1:
+    print(i)
+    if i >= 5:
+        print('Breaking')
+print('Finish')
+
+y = 1
+
+while y <=5:
+    print(y)
+    y+=1
+    if y ==2:
+        print("Statement")
+        continue
+
+amount_of_money = 1
+
+while amount_of_money != 10:
+    amount_of_money+=1
+    print("I received" +" " + "$" + str(amount_of_money))
